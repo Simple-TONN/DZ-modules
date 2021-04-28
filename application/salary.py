44974438e0b0c62a59fe05e62f5b75fc373e4e3c
Anton Fursov <simpletonn@live.ru>
2021-04-28 22:28:25 +0300

    done

---
+++ application/salary.py
@@ -0,0 +1,5 @@
+def calculate_salary():
+    print('calculate_salary')
+
+if __name__ == '__main__':
+    print('salary.py')
---
+++ dirty_main.py
@@ -0,0 +1,8 @@
+from application.salary import *
+from application.db.people import *
+import datetime as dt
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(dt.datetime.now())
